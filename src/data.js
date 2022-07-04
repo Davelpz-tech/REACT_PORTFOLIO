3cd8f5210eb0f68ae239182ead387036056e91c5
Davelpz-tech <davelpz15@gmail.com>
2022-07-04 16:55:52 -0700

    Adding links and changing products

--- src/data.js
+++ src/data.js
@@ -1,17 +1,21 @@
+import jsq from './img/javascript-quiz-answer-correct.PNG'
+import fg from './img/foodguru.png'
+import hcg from './img/hcg-mock.PNG'
+
 export const products = [
     {
         id: 1,
-        img: "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
-        link: "https://github.com/Davelpz-tech"
+        img: hcg,
+        link: "https://young-badlands-81611.herokuapp.com/"
     },
     {
         id: 2,
-        img: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1598&q=80",
-        link: "https://github.com/Davelpz-tech"
+        img: jsq,
+        link: "https://davelpz-tech.github.io/JavaScript-Code-Quiz/"
     },
     {
         id: 3,
-        img: "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80",
-        link: "https://github.com/Davelpz-tech"
-    },
+        img: fg,
+        link: "https://github.com/jlentz17/food-guru"
+    }
 ]
